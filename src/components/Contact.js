cf6a4f58f88aeeb3087bb06e6a30ffaf60a08a25
Laviza Falak Naz <60230431+LavizaFalakNaz@users.noreply.github.com>
2021-03-20 00:47:05 +0500

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './contact.css';
+
+function Contact()
+{
+  return(
+      <div className="content">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <a href="https://web.facebook.com/lavizakhan.niazi" class="fa fa-facebook"/>
+        <a href="https://twitter.com/NazLaviza" class="fa fa-twitter"/>
+        <a href="mailto:lavizaniazi2001@gmail.com" class="fa fa-google"/>
+        <a href="https://www.linkedin.com/in/lavizafalaknaz/" class="fa fa-linkedin"/>
+        <a href="https://www.instagram.com/lavizaniazi/" class="fa fa-instagram"/>
+      </div>
+  );
+}
+
+export default Contact;
