484f75acaab4753224282343ddcebf11ccf33824
Laviza Falak Naz <60230431+LavizaFalakNaz@users.noreply.github.com>
2021-03-21 19:43:48 +0500

    Create education.js

---
+++ src/components/education.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./education.css";
+
+function Education()
+{
+  return(
+    <div className="content">
+      <div className="Edu">
+        <h3>NED University of Engineering and Technology</h3>
+        <h5>B.E. Software Engineering --  Oct 2018 - Sept 2022</h5>
+        <h3>DA College for Women</h3>
+        <h5>F.Sc. Pre-Engineering -- Aug 2016 - July 2018</h5>
+        <h3>Aga Khan School Kharadar</h3>
+        <h5>S.Sc. Scienve -- Mar 2004 - May 2016</h5>
+      </div>
+    </div>
+  );
+}
+
+export default Education;
